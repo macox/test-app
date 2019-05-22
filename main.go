cd7baf584dfd5addada1fb04daeac9f69e022175
Mark Cox <markcox20@hotmail.com>
2019-05-22 10:13:15 +0000

    Blahs

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X example blah blah"
+	title := "Jenkins X example blah blahs"
 
 	from := ""
 	if r.URL != nil {
